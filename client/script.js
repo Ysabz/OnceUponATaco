edd7921bfbf9726018c37fcabe9fe4dab6b5523d
Narges <Nargessabbagh@yahoo.com>
2023-02-05 02:00:47 -0500

    changing script.js

--- client/script.js
+++ client/script.js
@@ -1,122 +1,12 @@
 import bot from './assets/bot.svg';
 import user from './assets/user.svg';
-import $ from "jquery";
-
 
 const form = document.querySelector('form');
+const span = document.querySelector('definitionBox');
 const chatContainer = document.querySelector('#chat_container');
 let loadInterval;
 let isLoaded = true;
 
-
-
-// handle message submission
-const handleSubmit = async (e) => {
-  isLoaded = false;
-  e.preventDefault();
-
-  const formData = new FormData(form);
-  // user's chatstripe
-  chatContainer.innerHTML += chatStripe(false, formData.get('prompt'));
-  form.reset();
-
-  chatContainer.scrollTop = chatContainer.scrollHeight;
-
-
-  // fetch data from server -> bot's response 
-  // const response = await fetch('https://aichatbottest.onrender.com', {
-  const response = await fetch('http://localhost:5000', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      prompt: formData.get('prompt'),
-      type: 'correction'
-    })
-  })
-
-  clearInterval(loadInterval);
-
-
-  if (response.ok) {
-    const data = await response.json();
-    const parsedData = data.bot.trim();
-    // check there was no grammar mistakes skip this step
-    if (parsedData.localeCompare(formData.get('prompt').trim()) != 0) {
-
-      // text correction chatstripe
-      const uniqueIdCorrector = generateUniqueId();
-      chatContainer.innerHTML += correctionChatStripe(" ", uniqueIdCorrector);
-
-
-
-      const messageDiv = document.getElementById(uniqueIdCorrector);
-
-      messageDiv.innerHTML = '';
-      typeText(messageDiv, "Correction:" + parsedData, formData, function () { handleCompletion(formData) });
-    }
-    else {
-      handleCompletion(formData);
-    }
-
-  } else {
-    const err = await response.text();
-
-    messageDiv.innerHTML = "Something went wrong";
-
-    alert(err);
-  }
-
-}
-
-// handle text completion
-const handleCompletion = async (formData) => {
-
-  //bot's chatstripe
-  const uniqueId = generateUniqueId();
-
-  chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
-
-  chatContainer.scrollTop = chatContainer.scrollHeight;
-
-  const messageDiv = document.getElementById(uniqueId);
-
-  loader(messageDiv);
-
-  // fetch data from server -> bot's response 
-
-  // const response = await fetch('https://aichatbottest.onrender.com', {
-  const response = await fetch('http://localhost:5000', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      prompt: formData.get('prompt'),
-      type: 'completion'
-    })
-  })
-
-  clearInterval(loadInterval);
-  messageDiv.innerHTML = '';
-
-  if (response.ok) {
-    const data = await response.json();
-    const parsedData = data.bot.trim();
-    typeText(messageDiv, parsedData);
-  } else {
-    const err = await response.text();
-
-    messageDiv.innerHTML = "Something went wrong";
-
-    alert(err);
-  }
-
-}
-
-
-// show text is loading
 function loader(element) {
   element.textContent = '';
 
@@ -130,9 +20,7 @@ function loader(element) {
   }, 300)
 }
 
-
-// type the text in message
-function typeText(element, text, data, callback) {
+function typeText(element, text) {
   let index = 0;
 
   let interval = setInterval(() => {
@@ -142,14 +30,11 @@ function typeText(element, text, data, callback) {
     }
     else {
       isLoaded = true;
-      clearInterval(interval);
-      if (callback) callback(data);
+      clearInterval(interval)
     }
   }, 20)
 }
 
-
-// generate unique id
 function generateUniqueId() {
   const timestamp = Date.now();
   const randomNumber = Math.random();
@@ -157,7 +42,6 @@ function generateUniqueId() {
 
   return `id-${timestamp}-${hexadecimalString}`
 }
-
 
 function chatStripe(isAi, value, uniqueId) {
   return (
@@ -178,97 +62,92 @@ function chatStripe(isAi, value, uniqueId) {
 }
 
 
-function correctionChatStripe(value, uniqueId) {
-  return (
-    `
-    <div class = "wrapper correction">
-    <div class= "chat">
-    <div class= "profile">
-    <img
-      src = "${bot}"
-      alt= "${'bot'}"
-    />
-    </div>
-    <div class="message" id = ${uniqueId}> ${value}</div>
-    </div>
-    </div>
-    `
-  )
+const handleSubmit = async (e) => {
+  isLoaded = false;
+  e.preventDefault();
+  const data = new FormData(form);
+  var message = data.get('prompt').trim()
+  if(message!=''){
+
+  // user's chatstripe
+  chatContainer.innerHTML += chatStripe(false, message);
+  form.reset();
+  if(message.toLowerCase()!='end'){
+  //bot's chatstripe
+
+  const uniqueId = generateUniqueId();
+  chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
+
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+
+  const messageDiv = document.getElementById(uniqueId);
+
+  loader(messageDiv);
+
+  // fetch data from server -> bot's response 
+
+  // const response = await fetch('https://aichatbottest.onrender.com', {
+  const response = await fetch('http://localhost:5000', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      prompt: data.get('prompt')
+    })
+  })
+
+  clearInterval(loadInterval);
+  messageDiv.innerHTML = '';
+
+  if (response.ok) {
+    const data = await response.json();
+    const parsedData = data.bot.trim();
+    typeText(messageDiv, parsedData);
+  } else {
+    const err = await response.text();
+
+    messageDiv.innerHTML = "Something went wrong";
+
+    alert(err);
+  }
+}else{
+  $('#endingModal').modal('show');
+}
+}
 }
 
-
-// on click event for selecting word on a single click
-$('#chat_container').on("click", async function (event) {
-  event.preventDefault();
-  $("span.popup-tag").css("display", "none");
-  if (isLoaded) {
-    const s = window.getSelection();
-    var range = s.getRangeAt(0);
-    var node = s.anchorNode;
-
-    // decrease start offset until a white space or puntation is reached or if reached the beginning of the text 
-    while (range.toString().indexOf(' ') != 0 && range.startOffset > 0 && !(!!range.toString().charAt(0).match(/^[.,:!?"]/))) {
-      range.setStart(node, range.startOffset - 1);
-    }
-
-    if (range.toString().indexOf(' ') == 0 || !!range.toString().charAt(0).match(/^[.,:!?"]/)) {
-      range.setStart(node, range.startOffset + 1);
-    }
-
-    while (
-      range.toString().indexOf(' ') == -1 &&
-      range.toString().trim() != '' &&
-      range.endOffset + 1 < s.baseNode.wholeText.length
-      &&
-      !(!!range.toString().charAt(range.toString().length - 1).match(/^[.,:!?"]/))
-    ) {
-      range.setEnd(node, range.endOffset + 1);
-    }
-
-
-    if (!!range.toString().charAt(range.toString().length - 1).match(/^[.,:!?"]/)) {
-      range.setEnd(node, range.endOffset - 1);
-    }
-
-    // remove extra space
-    range.setEnd(node, range.endOffset);
-    var str = range.toString().trim();
-    // remove last selection if is not letter or number
-    const lastChar = range.toString().charAt(range.toString().length - 1);
-    if (!/^[a-zA-Z0-9]*$/.test(lastChar)) {
-      range.setEnd(node, range.endOffset - 1);
-    }
-    if (/^[a-zA-Z]*$/.test(range.toString())) {
-      str = range.toString().trim();
-      if (str != '') {
-        str = await getDefinition(str);
-        $("span.popup-tag").css("display", "block");
-        $("span.popup-tag").css("top", event.clientY);
-        $("span.popup-tag").css("left", event.clientX);
-        $("span.popup-tag").text(str);
-      } else {
-        $("span.popup-tag").css("display", "none");
-      }
-
-    }
-
+form.addEventListener('submit', handleSubmit);
+form.addEventListener('keyup', (e) => {
+  if (e.keyCode === 13) {
+    handleSubmit(e);
   }
+})
 
-});
+function getSelected() {
+  if (window.getSelection) { return window.getSelection(); }
+  else if (document.getSelection) { return document.getSelection(); }
+  else {
+    var selection = document.selection && document.selection.createRange();
+    if (selection.text) { return selection.text; }
+    return false;
+  }
+  return false;
+}
 
-
+function getSound() {
+  return document.getElementById('checkboxsound').checked
+}
 async function getDefinition(word) {
   const dictionaryKey = "55e4d7e5-002a-4ac6-87a3-c65d9c3323e8"
-  const response = await fetch(`https://dictionaryapi.com/api/v3/references/learners/json/${word}?key=${dictionaryKey}`);
+  const response = await fetch(`https://dictionaryapi.com/api/v3/references/learners/json/${word}?key=${dictionaryKey}`)
   const data = await response.json();
-  if (!data.length) { return "Not found" }
-  if (!data[0].shortdef) {
-    return "Not found"
-
-  }
+  console.log(data);
+  if (!data.length) { return "Couldn't Find The Word" }
+  if (!data[0].shortdef[0]) { return '' }
   var definition = data[0].shortdef[0]
   try { var audio = data[0].hwi.prs[0].sound.audio } catch { }
-  if (audio) {
+  if (getSound() && audio) {
     soundRender(audio, dictionaryKey)
   }
   return definition
@@ -282,11 +161,77 @@ function soundRender(audio, apikey) {
   aud.controls = true
   aud.setAttribute("autoplay", '')
 }
+/* create sniffer */
 
+/**$('#chat_container').mouseup(async function (event) {
+  var selection = getSelected();
+  console.log(selection.toString());
+  selection = $.trim(selection);
 
-form.addEventListener('submit', handleSubmit);
-form.addEventListener('keyup', (e) => {
-  if (e.keyCode === 13) {
-    handleSubmit(e);
+  if (selection != '') {
+    var definition = await getDefinition(selection)
+    $("span.popup-tag").css("display", "block");
+    $("span.popup-tag").css("top", event.clientY);
+    $("span.popup-tag").css("left", event.clientX);
+    $("span.popup-tag").text(definition);
+  } else {
+    $("span.popup-tag").css("display", "none");
   }
-})
+});**/
+
+// TODO fix the bug if the last element on the line is selected (DONe)
+// TODO disable clicking while the bot is typing
+// TODO has a bug on testing 123
+$('#chat_container').click(async function (event) {
+  $("span.popup-tag").css("display", "none");
+  if (isLoaded) {
+    const s = window.getSelection();
+    var range = s.getRangeAt(0);
+    var node = s.anchorNode;
+
+    while (range.toString().indexOf(' ') != 0) {
+      range.setStart(node, range.startOffset - 1);
+    }
+
+    range.setStart(node, range.startOffset + 1);
+
+    while (
+      range.toString().indexOf(' ') == -1 &&
+      range.toString().trim() != '' &&
+      range.endOffset + 1 < s.baseNode.wholeText.length
+    ) {
+      range.setEnd(node, range.endOffset + 1);
+    }
+
+    // remove extra space
+    range.setEnd(node, range.endOffset);
+    var str = range.toString().trim();
+    console.log("testing", str);
+    // remove last selection if is not letter or number
+    const lastChar = range.toString().charAt(range.toString().length - 1);
+    if (!/^[a-zA-Z0-9]*$/.test(lastChar)) {
+      range.setEnd(node, range.endOffset - 1);
+    }
+    if (/^[a-zA-Z]*$/.test(range.toString())) {
+      str = range.toString().trim();
+      console.log(str);
+      if (str != '') {
+        str = await getDefinition(str)
+        if (str != '') {
+          $("span.popup-tag").css("display", "block");
+          $("span.popup-tag").css("top", event.clientY);
+          $("span.popup-tag").css("left", event.clientX);
+          $("span.popup-tag").text(str);
+        }
+
+      } else {
+        $("span.popup-tag").css("display", "none");
+      }
+
+    }
+
+  }
+
+});
+
+
